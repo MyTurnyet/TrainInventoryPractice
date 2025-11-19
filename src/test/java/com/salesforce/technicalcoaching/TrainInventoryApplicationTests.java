00c258863a0ad2d6ce4b3f039afe9a12f42f36dc
paige.watson <paige.watson@salesforce.com>
2025-11-19 18:00:43 +0530

    initial commit

---
+++ src/test/java/com/salesforce/technicalcoaching/TrainInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesforce.technicalcoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
